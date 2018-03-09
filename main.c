0c1286bc324ccc9f7993b38821de1ee26b2ecfb6
Felipe <ffraporti@gmail.com>
2018-03-09 13:52:04 -0300

    First Commit
    Sync commit for the beginning of the project.

---
+++ main.c
@@ -0,0 +1,11 @@
+/**
+ * 	PWC - A module used to control power source drivers.
+ *
+ *	Author: Felipe Fraporti - ffraporti@gmail.com
+ *	Contributors: Breno Cardoso
+ */
+
+int main() {
+
+	return 1;
+}
